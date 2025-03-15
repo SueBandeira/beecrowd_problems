77ef09d493c7c3a870f385bfa0fb6910b25787f3
SueBandeira <sbandeira099@gmail.com>
2025-03-15 13:25:28 -0300

    tests 1001 and 1002

--- beecrowd/src/beginner_iniciante/Main1001.java
+++ beecrowd/src/beginner_iniciante/Main1001.java
@@ -1,5 +1,7 @@
 package beginner_iniciante;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Scanner;
 
 public class Main1001 {
@@ -7,8 +9,30 @@ public class Main1001 {
     Scanner sc = new Scanner(System.in);
     int A= sc.nextInt(), B= sc.nextInt(), X;
 
-    X = A + B;
+    X = soma(A, B);
 
     System.out.printf("X = %d\n", X);
+  }
+  public static int soma(int a, int b) {
+    return a + b;
+  }
+}
+
+class Main1001Test {
+  @Test
+  void solution() {
+    int[][] input = {
+        {10, 9},
+        {-10, 4},
+        {15, -7}
+    };
+
+    int[] expected = {19, -6, 8};
+
+    for (int i = 0; i < input.length; i++) {
+      int result = Main1001.soma(input[i][0], input[i][1]);
+      assertEquals(expected[i], result,
+          "Erro na soma de " + input[i][0] + " + " + input[i][1]);
+    }
   }
 }
--- beecrowd/src/beginner_iniciante/Main1002.java
+++ beecrowd/src/beginner_iniciante/Main1002.java
@@ -1,14 +1,39 @@
 package beginner_iniciante;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Map;
 import java.util.Scanner;
+
 
 public class Main1002 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     double A, R=sc.nextDouble();
 
-    A =  Math.PI * Math.pow(R, R);
+    A = area(R);
 
     System.out.printf("A=%.4f\n", A);
+  }
+
+  public static double area(double raio) {
+    //return Math.PI * Math.pow(raio, 2); PI com mais casas decimais, usar o valor 3,14159 como explicitado no problema
+    return 3.14159 * Math.pow(raio, 2);
+  }
+}
+class Main1002Test {
+  @Test
+  void solution() {
+    Map<Double, Double> expectedValues = Map.of(
+        2.0, 12.5664,
+        100.64, 31819.3103,
+        150.00, 70685.7750
+    );
+
+    for (Map.Entry<Double, Double> entry : expectedValues.entrySet()) {
+      double result = Main1002.area(entry.getKey());
+      assertEquals(entry.getValue(), result, 0.0001,
+          "Erro na área do círculo de raio " + entry.getKey());
+    }
   }
 }
